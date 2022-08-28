a9008881a9786c033f4f695f5377d2f01c414f41
unknown <tfb1223@gmail.com>
2022-08-28 00:35:25 -0500

    updated shop options

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,14 +22,14 @@ var fightOrSkip = function () {
             return true;
         }
     }
-}
+};
 // var to fight robot's
 var fight = function (enemy) {
     console.log(enemy);
     while (playerInfo.health > 0 && enemy.health > 0) {
         if (fightOrSkip()) {
             break;
-        }
+        };
         // remove enemy's health by subtracting playerInfo.attack variable
         var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
         enemy.health = Math.max(0, enemy.health - damage);
@@ -101,19 +101,17 @@ var endGame = function () {
 };
 var shop = function () {
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     switch (shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
             break;
         default:
